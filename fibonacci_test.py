8ba3f9f77d39d29cefdb09f4a4494cecbade4547
Martin <h400747@gmail.com>
2024-12-19 22:59:46 +0100

    added decorator

--- fibonacci_test.py
+++ fibonacci_test.py
@@ -1,3 +1,17 @@
+import time
+
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with arguments {args} {kwargs}")
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} returned {result}")
+        print(f"Execution time: {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+@log_decorator
 def fibonacci(n):
     fib_sequence = [0, 1]
     while len(fib_sequence) < n:
@@ -7,4 +21,3 @@ def fibonacci(n):
 # Example usage:
 n = 10  # Change this value to generate more or fewer Fibonacci numbers
 print(fibonacci(n))
-
